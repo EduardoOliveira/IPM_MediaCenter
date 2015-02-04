23c682e68ae91e663f3ad3d22cf2037d100ae662
Eduardo Oliveira <eduardoliveira2009@gmail.com>
2015-02-04 16:00:57 +0000

    Added IMDBScrapper FIX

---
+++ scrapper/IMDBtop250.js
@@ -0,0 +1,15 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var imdbMovieScrapper = require('./imdbMovieScrapper');
+var baseURL = "http://akas.imdb.com";
+var listPath = "/chart/top";
+
+request(baseURL+listPath, function (err, resp, body) {
+        $ = cheerio.load(body);
+        var scrapper = new imdbMovieScrapper(baseURL);
+        $("tbody.lister-list td.titleColumn a").each(function(){
+            scrapper.scrapMovie(this.attribs.href);
+        });
+});
+
+
