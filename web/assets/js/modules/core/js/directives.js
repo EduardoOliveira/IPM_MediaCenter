7dbb61b089b8b7817a688c37394819e2ecb5c718
Eduardo Oliveira <eduardoliveira2009@gmail.com>
2015-02-18 16:59:46 +0000

    BugFixes

--- web/assets/js/modules/core/js/directives.js
+++ web/assets/js/modules/core/js/directives.js
@@ -1,11 +1,4 @@
 'use strict';
 
-angular.module('mediaCenter.core.directives', [])
-    .directive('listComponent', function () {
-        return {
-            controller:'ListController',
-            restrict: 'E',
-            templateUrl: '/assets/js/modules/core/templates/listComponent.html'
-        }
-    });
+angular.module('mediaCenter.core.directives', []);
 
