f5000d6976c1cb9fba9c5d392d58f8f0774f1ab4
Eduardo Oliveira <eduardoliveira2009@gmail.com>
2015-02-23 19:06:54 +0000

    File name fix

--- src/main/java/pt/iscte/ipm/mediacenter/mediahandler/MediaHandler.java
+++ src/main/java/pt/iscte/ipm/mediacenter/mediahandler/MediaHandler.java
@@ -2,6 +2,6 @@ package pt.iscte.ipm.mediacenter.mediahandler;
 
 import java.io.File;
 
-public interface Mediahandler2 extends Runnable{
+public interface MediaHandler extends Runnable{
     public void handle(File f);
 }
