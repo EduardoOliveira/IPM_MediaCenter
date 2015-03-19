a7281a37e035f89f030f0848ee5111f0fc2d26d2
Eduardo Oliveira <eduardoliveira2009@gmail.com>
2015-03-19 01:16:17 +0000

    Platform sync

---
+++ src/main/java/pt/iscte/ipm/mediacenter/remote/handling/PlayBackDeviceSelectionEventHandler.java
@@ -0,0 +1,12 @@
+package pt.iscte.ipm.mediacenter.remote.handling;
+
+import org.eclipse.jetty.websocket.api.Session;
+import pt.iscte.ipm.mediacenter.core.events.Event;
+import pt.iscte.ipm.mediacenter.core.events.EventHandler;
+
+public class PlayBackDeviceSelectionEventHandler implements EventHandler{
+    @Override
+    public void handle(Event event, Session session) {
+
+    }
+}
