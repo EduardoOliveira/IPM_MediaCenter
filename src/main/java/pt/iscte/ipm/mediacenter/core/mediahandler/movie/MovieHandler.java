5e7210359253067bac1f672fa74924e158c4639f
juanvaldivia <jsmederos@gmail.com>
2015-05-08 19:46:50 +0100

    Dependencias themoviedbapi

--- src/main/java/pt/iscte/ipm/mediacenter/core/mediahandler/movie/MovieHandler.java
+++ src/main/java/pt/iscte/ipm/mediacenter/core/mediahandler/movie/MovieHandler.java
@@ -1,15 +1,18 @@
 package pt.iscte.ipm.mediacenter.core.mediahandler.movie;
 
+import pt.iscte.ipm.mediacenter.core.database.movie.MovieDAO;
 import pt.iscte.ipm.mediacenter.core.mediahandler.MediaHandler;
+import com.omertron.themoviedbapi.TheMovieDbApi;
 
 import java.nio.file.Path;
 
 public class MovieHandler implements MediaHandler {
+
+    MovieDAO movieDAO = new MovieDAO();
+
     @Override
     public void handle(Path path) {
         System.out.println(path.toString());
-
-
-
     }
+
 }
