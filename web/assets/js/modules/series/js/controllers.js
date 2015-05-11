1b4c3002caa0c9cf690eebcdbc19a892a7713670
Eduardo Oliveira <eduardoliveira2009@gmail.com>
2015-05-11 22:19:30 +0100

    Series views Media improvements

--- web/assets/js/modules/series/js/controllers.js
+++ web/assets/js/modules/series/js/controllers.js
@@ -1,14 +1,18 @@
 'use strict';
 
 angular.module('mediaCenter.series.controllers', [])
-    .controller('SeriesManagerController', function ($scope,SeriesListService,BackgroundImageService) {
-        $scope.$watch( function () { return SeriesListService.selected; }, function (data) {
-            $scope.selected = SeriesListService.selected;
+    .controller('SeriesManagerController', function ($scope,SeriesListService,BackgroundImageService,$stateParams,SeriesService,$filter) {
+        var series = SeriesService.series;
+        console.log($stateParams);
+        var selected = $filter('filter')(series,$stateParams,true);
+        if(selected.length > 0){
+            selected = selected[0];
+            $scope.selected = selected;
             if($scope.selected.posterImages != undefined){
                 var size = $scope.selected.posterImages.length;
                 $scope.poster = $scope.selected.posterImages[Math.floor(Math.random() * size)].webPath;
                 size = $scope.selected.fanArtImages.length;
                 BackgroundImageService.image = $scope.selected.fanArtImages[Math.floor(Math.random() * size)].webPath;
             }
-        }, true);
+        }
     });
